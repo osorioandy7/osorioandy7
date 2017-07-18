6f8749ba6565b668e005618e6587bc0e8af27fe0
Demo User <demouser@MacBook-Air.local>
2017-07-18 09:26:44 -0700

    Going in with Welcome and Redirect

--- fadeNameOut.js
+++ fadeNameOut.js
@@ -3,10 +3,26 @@ var userName;
 //Testing hiding the post name box
 function hideNameBox(){
 $("#name_box").hide();
+$('#welcomeMessage').hide();
+$('#redirectMessage').hide();
+}
+//Fades in the welcome message
+function fadeWelcomeIn(){
+  $('#welcomeMessage').fadeIn(4000);
+  $('#welcomeMessage').fadeOut(500);
+}
+function fadeInRedirect(){
+
+$('#redirectMessage').fadeIn(4000);
+
+
 }
 //Fades new text box into screen
 function fadeNameIn(){
-$('#name_box').fadeIn(25000);
+$('#name_box').fadeIn(4000);
+$('#name_box').fadeOut(500);
+
+
 }
 //Fades screen
 function fadeScreen(){
@@ -22,7 +38,9 @@ $('button').on('click',fadeScreen);
 function rightUserNameInVar(){
 userName = $("textarea").val();
 $('#name_box').append(userName);
-fadeNameIn();
+setTimeout(fadeNameIn,2000);
+setTimeout(fadeWelcomeIn,6500);
+setTimeout(fadeInRedirect,11000);
 return userName;
 }
 //Prepares button for pressing//
